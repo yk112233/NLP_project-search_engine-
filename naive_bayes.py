cc60c19f0b564db9cb3b0efdf5e9b5a9c4e5feab
babyyang525 <60056145+babyyang525@users.noreply.github.com>
2020-11-02 22:49:45 +0800

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -34,57 +34,6 @@ def process(sentence):
 stoplist = './stopword.txt'
 stop = pd.read_csv(stoplist, encoding = 'utf-8', header = None, sep = 'tipdm',engine='python')
 stop = [' ', ''] + list(stop[0])  # Pandas自动过滤了空格符，这里手动添加（在每条数据的开头加个空格）
-'''
-filename='./评论数据集.csv'
-corpus=[]
-grade=[]
-with open(filename,'rt',encoding='gbk') as f:
-    Readers=reader(f)
-    Readers=list(Readers)
-for readers in Readers:
-    corpus.append(readers[0][0:len(readers[0])-1])
-    grade.append(readers[2])
-train_x, test_x, train_y, test_y = train_test_split(corpus, grade, random_state=0, test_size=0.2)
-print(type(train_y[0]))
-print(train_y)
-list_post=[]
-test_list_post=[]
-for sentence in train_x:
-    list_post.append(process(sentence))
-for sentence in test_x:
-    test_list_post.append(process(sentence))
-dictionary=np.asarray(get_word(list_post))
-dictionary, counts = np.unique(dictionary, return_counts=True)
-freq, dictionary = (list(i) for i in zip(*(sorted(zip(counts,dictionary), reverse=True))))
-dictionary=dictionary[0:6000]#6684
-trainMat=[]
-for i in range(len(list_post)):
-    postinDoc=list_post[i]
-    trainMat.append(setOfWords2Vec(dictionary, postinDoc))
-with open('训练矩阵.txt','a') as file_handle:
-    for i in (trainMat):
-        for j in i:
-            file_handle.write(str(j))
-        file_handle.write('\n')
-# with open('train_y.txt','a') as file_handle:
-#     for i in train_y:
-#         file_handle.write(str(i))
-#         # file_handle.write(" ")
-# with open('dictionary.txt','a') as file_handle:
-#     for i in dictionary:
-#         file_handle.write(str(i))
-#         file_handle.write(" ")
-from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB()
-clf.fit(trainMat, train_y)
-X_test=[]
-for i in tqdm(range(len(test_list_post))):
-    testEntry=test_list_post[i]
-    X_test.append(np.array(setOfWords2Vec(dictionary, testEntry)))
-
-Y_predict = clf.predict(X_test)
-print(classification_report(test_y, Y_predict))
-'''
 header_dict = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Accept-Language": "zh-CN,zh;q=0.9",
@@ -135,15 +84,7 @@ def get_comment(url):
         comments = jobj["data"]['list']
         for comment in comments:
             pinglun.append(deEnglish(comment['info']['content']))
-    with open('pinglun.txt', 'a') as file_handle:
-        for i in pinglun:
-            file_handle.write(i)  # 写入
-            file_handle.write('\n')  # 有时放在循环里面需要自动转行，不然会覆盖上一条数据
-    pinglun_txt = open('pinglun.txt', 'r', encoding="gbk").readlines()
-    comment_sentence=[]
-    for i in pinglun_txt:
-        comment_sentence.append(i)
-    return comment_sentence
+    return pinglun
 
 def emotion(url):
     comments=get_comment(url)
