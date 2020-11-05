bdc936f3137c2a462ea9dbca7178cdd5e874ac39
vicwtor <73054329+vicwtor@users.noreply.github.com>
2020-11-05 18:00:30 +0800

    Add files via upload

---
+++ naive_bayes.py
@@ -0,0 +1,121 @@
+
+from csv import reader
+import jieba
+import time
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import re
+import json
+from urllib import request
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]=1
+    return returnVec
+def get_word(list):
+    new_list = []
+    for i in list:
+        for j in i:
+            new_list.append(j)
+    return new_list
+def process(sentence):
+    wordlist=[]
+    for word in list(jieba.cut(sentence, cut_all=False)):
+        if word not in stop:
+            wordlist.append(word)
+    return wordlist
+stoplist = './stopword.txt'
+stop = pd.read_csv(stoplist, encoding = 'utf-8', header = None, sep = 'tipdm',engine='python')
+stop = [' ', ''] + list(stop[0])  # Pandas自动过滤了空格符，这里手动添加（在每条数据的开头加个空格）
+header_dict = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+}
+
+def get_http(load_url, header=None):
+    res = ""
+    try:
+        req = request.Request(url=load_url, headers=header)  # 创建请求对象
+        coonect = request.urlopen(req)  # 打开该请求
+        byte_res = coonect.read()  # 读取所有数据，很暴力
+        try:
+            res = byte_res.decode(encoding='utf-8')
+        except:
+            try:
+                res = byte_res.decode(encoding='gbk')
+            except:
+                res = ""
+    except Exception as e:
+        print(e)
+    return res
+
+
+def deEnglish(sentence):
+    new_sentence = ""
+    for word in sentence:
+        if u'\u4e00' <= word <= u'\u9fff':
+            new_sentence += word
+    return new_sentence
+def get_comment(url):
+    match = re.match(r'https://blog.csdn.net/(.*)/article/details/([0-9]*)(.*)', url)
+    num_id = match.group(2)
+    url_comment = "https://blog.csdn.net/phoenix/web/v1/comment/list/" + num_id + "?page=1&size=10&commentId="
+    res = get_http(url_comment, header_dict)
+    jobj = json.loads(res)  # 解析json
+    page_count = jobj["data"]['pageCount']
+    pinglun = list()
+    for page in range(page_count):
+        url_page = "https://blog.csdn.net/phoenix/web/v1/comment/list/" + num_id + "?page=" + str(
+            page) + "&size=10&commentId="
+        res = get_http(url_page, header_dict)
+        time.sleep(0.1)
+        if res == None or len(res) <= 30:
+            print("加载错误", url)
+            continue
+        jobj = json.loads(res)  # 解析json
+        comments = jobj["data"]['list']
+        for comment in comments:
+            pinglun.append(deEnglish(comment['info']['content']))
+    return pinglun
+
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+def emotion(url):
+    comments=get_comment(url)
+    from sklearn.naive_bayes import MultinomialNB
+    clf = MultinomialNB()
+    text = open('训练矩阵.txt').readlines()
+    trainMat=[]
+    for line in text:
+        row=[]
+        for i in range(len(line)-1):
+            row.append(int(line[i]))
+        trainMat.append(row)
+    text2 = open('train_y.txt','r', encoding="gbk").readlines()
+    train_y=[]
+    for i in text2:
+        for j in range(len(i)):
+            train_y.append(int(i[j]))
+    dictionary=[]
+    text3 = open('dictionary.txt', 'r', encoding="utf-8").readlines()
+    text3=str(text3[0])
+    for i in text3.split():
+        dictionary.append(i)
+    clf.fit(trainMat, train_y)
+    x=[]
+    for s in comments:
+        s=np.array(setOfWords2Vec(dictionary, process(s)))
+        x.append(s)
+    if len(x)==0:
+        return 0
+    y_predict=clf.predict(x)
+    average_score=y_predict.sum()/len(y_predict)
+    average_score = round(average_score, 2)
+    return average_score
---
+++ project_part2.py
@@ -0,0 +1,110 @@
+#  建索引
+#from docx import Document
+import jieba
+import math
+import operator
+
+class Document:
+    def __init__(self, key, title, neirong, link):
+        self.dict = {'key':key, 'title':title, 'neirong':neirong, 'link':link}
+
+    def get(self, index):
+        for first in self.dict:
+            if index == first:
+                return self.dict[first]
+
+        return None
+
+
+
+class Indexer:
+    inverted = {}  # 记录词所在文档及词频
+    idf = {}  # 词的逆文档频率
+    id_doc = {}  # 文档与词的对应关系
+
+    def __init__(self, file_path):
+        self.doc_list = []
+        self.index_writer(file_path)
+
+    def index_writer(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                key, title, neirong, link = line.strip().split('\t\t\t')
+                doc = Document(key, title, neirong, link)
+                self.doc_list.append(doc)
+        self.index()
+
+    def index(self):
+        doc_num = len(self.doc_list)  # 文档总数
+        for doc in self.doc_list:
+            key = doc.get('key')
+            # 正排
+            self.id_doc[key] = doc
+
+            # 倒排
+            term_list = list(jieba.cut_for_search(doc.get('neirong')))  # 分词
+            for t in term_list:
+                if t in self.inverted:
+
+                    if key not in self.inverted[t]:
+                        self.inverted[t][key] = 1/len(term_list)
+                    else:
+                        self.inverted[t][key] += 1/len(term_list)
+                else:
+                    self.inverted[t] = {key: 1/len(term_list)}
+
+        for t in self.inverted:
+            self.idf[t] = math.log10(doc_num / (1+len(self.inverted[t])))
+
+        print("inverted terms:%d" % len(self.inverted))
+        print("index done")
+        print(self.inverted)
+
+
+"""
+搜索
+返回结果：(相关问题,相似度)列表
+搜索步骤：
+    1.分词
+    2.计算tf-idf,找出候选doc
+    3.对文档排序
+"""
+
+
+class Searcher:
+
+    def __init__(self, index):
+        self.index = index
+
+    def search(self, query):
+        term_list = []
+        query = query.split()
+        for entry in query:
+            # 分词
+            term_list.extend(jieba.cut_for_search(entry))
+
+        # 计算tf-idf,找出候选doc
+        tf_idf = {}
+        for term in term_list:
+            if term in self.index.inverted:
+                for doc_id, fre in self.index.inverted[term].items():
+                    if doc_id in tf_idf:
+                        tf_idf[doc_id] += fre * self.index.idf[term]
+                    else:
+                        tf_idf[doc_id] =  fre * self.index.idf[term]
+        # 排序
+        sorted_doc = sorted(tf_idf.items(), key=operator.itemgetter(1), reverse=True)
+        print("sorted_tf_idf:")
+        print(sorted_doc)
+        res = [self.index.id_doc[doc_id] for doc_id, score in sorted_doc]
+        return res
+
+
+if __name__ == '__main__':
+    print("index")
+    index = Indexer("docs.txt")
+    searcher = Searcher(index)
+    print("0")
+    searcher.search("深度学习 CNN 蜂巢")
+    print("1")
+    print(len(index.doc_list))
